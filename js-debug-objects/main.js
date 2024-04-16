4c99b35912d6ca7e2ed02fa0c253908a9c32c33e
ryampeguero <ryampeguero@gmail.com>
2024-04-16 16:51:19 +0200

    feat: js-debug-object fixed

--- js-debug-objects/main.js
+++ js-debug-objects/main.js
@@ -56,7 +56,7 @@ const cars = [
         manufacturer: 'Seat',
         model: 'Ibiza',
         type: 'metano'
-    }
+    },
     {
         manufacturer: 'Audi',
         model: 'R8',
@@ -64,14 +64,14 @@ const cars = [
     },
 ];
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+const gasolineCars = cars.filter( (auto) => auto.type.toLowerCase() === 'benzina');
 
 const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
+    return auto.type.toLowerCase() === 'diesel';
 });
 
 const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
+    return auto.type.toLowerCase() !== 'benzina' && auto.type.toLowerCase() !== 'diesel';
 });
 
 console.log('Auto a benzina');
@@ -84,3 +84,9 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+//Mancava una virgola all'interno dell'array di object
+//portiamo al lowercase i dati nelle proprietà di type per fare i confronti
+//C'era un errore nel arrow function in riga 67 il simbolo maggiore va dopo uguale
+//Se scriviamo le condizioni all'interno delle parentesi graffe abbiamo bisogno del return nella riga 70
+//Ci va and al posto di or nella riga 74
